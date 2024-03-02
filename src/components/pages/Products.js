1ffdc65b245f444ad91bd409fa43b8a00e04263f
shravyaalake <137767583+shravyaalake@users.noreply.github.com>
2024-03-02 15:43:18 +0530

    Add files via upload

---
+++ src/components/pages/Products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Products() {
+  return <h1 className='products'>
+     For more information, refer :
+     <br>
+     </br>
+    <div>
+      <a href="https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwiR86fPqdWEAxXCxkwCHdaHAs0YABAAGgJ0bQ&ase=2&gclid=CjwKCAiAuYuvBhApEiwAzq_Yie6AIVouc1eHDulhrtoOp3RLkhzApI2Ce70rju8Z6Dp_vixfKzOOYBoCQW4QAvD_BwE&ohost=www.google.com&cid=CAESVeD2QoR_XTgM0XeKDGnho0XCDh4jRdNNuNblrj_gWJ5vaQpYl13AkV5KN3M2V7LU_rJB2R3ITMKMW2PM6o1Yg6R7Hp16hvDs_EN4NFhY5B1---EPoAs&sig=AOD64_17rBUBDmqbOEzmUpfbreWaT__4Pg&q&nis=4&adurl&ved=2ahUKEwjQ1aLPqdWEAxXhk68BHUHuDCAQ0Qx6BAgKEAE">travel website</a>
+    </div>
+  </h1>;
+}
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function SignUp() {
+  return <h1 className='sign-up'>THANK YOU! FOR SIGNING UP</h1>;
+}
